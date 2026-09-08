d7731577da55cb8836d3352fc97683b990b9be03
Daniel <davergaraf@unimagdalena.edu.co>
2026-09-07 23:09:52 -0500

    fix: add optional derived query

--- src/main/java/edu/coursehub/persistence/student/StudentProfileRepository.java
+++ src/main/java/edu/coursehub/persistence/student/StudentProfileRepository.java
@@ -2,6 +2,8 @@ package edu.coursehub.persistence.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
-    // TODO-STUDENT S05
+    Optional<StudentProfile> findByStudent_Id(Long studentId);
 }
